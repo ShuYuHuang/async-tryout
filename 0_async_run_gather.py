f271c6d98126b0832ec4daada1c1a42473d1043e
ShuYuHuang <b123767195@gmail.com>
2024-01-31 13:17:58 +0400

    altered the test contents for gathering from async run

--- 0_async_run_gather.py
+++ 0_async_run_gather.py
@@ -1,11 +1,28 @@
 import asyncio
 from time import time
+from time import sleep
 
-async def simulate_task(task_name, duration):
+async def asimulate_task(task_name, duration):
     print(f"{task_name} started")
     await asyncio.sleep(duration)
     print(f"{task_name} completed after {duration} seconds")
+
+async def simulate_task(task_name, duration):
+    print(f"{task_name} started")
+    sleep(duration)
+    print(f"{task_name} completed after {duration} seconds")
     
+async def amain():
+    # Gather all tasks to run concurrently
+    tasks = [
+        asimulate_task("Task 1", 2),
+        asimulate_task("Task 2", 1),
+        asimulate_task("Task 3", 3)
+    ]
+
+    # Run tasks concurrently
+    await asyncio.gather(*tasks)
+
 async def main():
     # Gather all tasks to run concurrently
     tasks = [
@@ -13,11 +30,14 @@ async def main():
         simulate_task("Task 2", 1),
         simulate_task("Task 3", 3)
     ]
-
-    # Run tasks concurrently
     await asyncio.gather(*tasks)
 
 # Run the event loop
 start_time = time()
+asyncio.run(amain())
+print(f"Run async function\ntakes {time()-start_time}s")
+
+start_time = time()
+# main()
 asyncio.run(main())
-print(f"takes{time()-start_time}s")
+print(f"Run sync function\ntakes {time()-start_time}s")
--- 2_async_request_gather_exp.py
+++ 2_async_request_gather_exp.py
@@ -10,11 +10,21 @@ async def call_asleep(session, task_name, duration):
         "duration": duration
     }
 
+    async with session.post("http://0.0.0.0:9527/asynsleep", data=pload) as response:
+        # You can handle the response if needed
+        print(await response.text())
+
+async def call_sleep(session, task_name, duration):
+    pload = {
+        "task_name": task_name,
+        "duration": duration
+    }
+
     async with session.post("http://0.0.0.0:9527/synsleep", data=pload) as response:
         # You can handle the response if needed
         print(await response.text())
 
-async def main():
+async def amain():
     async with aiohttp.ClientSession() as session:
         # Gather all tasks to run concurrently
         tasks = [
@@ -26,9 +36,24 @@ async def main():
         # Run tasks concurrentl
         await asyncio.gather(*tasks)
 
+async def main():
+    async with aiohttp.ClientSession() as session:
+        # Gather all tasks to run concurrently
+        tasks = [
+            call_sleep(session, "Task 1", 3),
+            call_sleep(session, "Task 2", 1),
+            call_sleep(session, "Task 3", 2)
+        ]
+
+        # Run tasks concurrentl
+        await asyncio.gather(*tasks)
 
 
 # Run the event loop
 start_time = time()
 asyncio.run(main())
-print(f"Takes {time()-start_time}s")
+print(f"Call sync function\nTakes {time()-start_time}s")
+
+start_time = time()
+asyncio.run(amain())
+print(f"Call async function\nTakes {time()-start_time}s")
